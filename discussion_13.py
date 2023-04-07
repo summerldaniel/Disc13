c579496bf883b61774fb2dad785a570890f92353
Summer Daniel <summerdaniel@Summers-MacBook-Air-3.local>
2023-04-07 19:45:48 -0400

    commit

--- discussion_13.py
+++ discussion_13.py
@@ -16,6 +16,7 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, job_id INTEGER, hire_date TEXT, salary NUMERIC")
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -31,6 +32,7 @@ def add_employee(filename, cur, conn):
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute("""INSERT OR IGNORE INTO employees (employee_id, first_name, last_name, job_id, hire_date, salary) VALUES (?, ?, ?, ?, ?, ?)""", (id, f_name, l_name, job, data, sal))
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
